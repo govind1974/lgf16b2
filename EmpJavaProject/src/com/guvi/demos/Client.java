5a81a0e1c5d1c94773ef0bae9b5c99fb0d0f00b9
govind <govind.corp.trainer@gmail.com>
2025-12-28 18:38:27 +0530

    added a new project

---
+++ EmpJavaProject/src/com/guvi/demos/Client.java
@@ -0,0 +1,13 @@
+package com.guvi.demos;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+
+	}
+
+}
